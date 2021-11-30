d66306a9a92fbec1dc71d1c789b88cf206c2bc82
Edgardo Mejia <edmejia272@gmail.com>
2021-11-30 01:24:57 -0600

    Interfaces Login

---
+++ app/src/main/java/com/example/entregadecomida/actualizarUsuario.java
@@ -0,0 +1,14 @@
+package com.example.entregadecomida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actualizarUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_usuario);
+    }
+}
