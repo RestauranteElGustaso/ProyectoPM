df4ad3f65c04f58d1603756759318d52008c971f
Edgardo Mejia <edmejia272@gmail.com>
2021-12-02 22:00:48 -0600

    Actualizar Cuenta y Diseño Login

--- app/src/main/java/com/example/entregadecomida/PerfilUsuario.java
+++ app/src/main/java/com/example/entregadecomida/PerfilUsuario.java
@@ -1,15 +1,18 @@
 package com.example.entregadecomida;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PerfilUsuario extends AppCompatActivity implements View.OnClickListener {
-Button btnEditar, btnEliminar, btnMostrar, btnSalir;
+Button btnEditar, btnEliminar, btnSalir;
 TextView nombre;
 int id=0;
 Usuario u;
@@ -21,11 +24,9 @@ daoUsuario dao;
         nombre=(TextView)findViewById(R.id.txtNombreUsuario);
         btnEditar=(Button)findViewById(R.id.btnEditarPerf);
         btnEliminar=(Button)findViewById(R.id.btnEliminarPerf);
-        btnMostrar=(Button)findViewById(R.id.btnMostrarPerf);
         btnSalir=(Button)findViewById(R.id.btnSalirPerf);
         btnEditar.setOnClickListener(this);
         btnEliminar.setOnClickListener(this);
-        btnMostrar.setOnClickListener(this);
         btnSalir.setOnClickListener(this);
 
         Bundle b = getIntent().getExtras();
@@ -41,13 +42,35 @@ daoUsuario dao;
         switch (v.getId()){
             case R.id.btnEditarPerf:
                 Intent a=new Intent(PerfilUsuario.this, actualizarUsuario.class);
+                a.putExtra("id", id);
                 startActivity(a);
                 break;
             case R.id.btnEliminarPerf:
-                break;
-            case R.id.btnMostrarPerf:
-                Intent b=new Intent(PerfilUsuario.this, MostarUsuarios.class);
-                startActivity(b);
+                AlertDialog.Builder b = new AlertDialog.Builder(this);
+                b.setMessage("¿Está seguro(a) que desea eliminar la cuenta?");
+                b.setCancelable(false);
+                b.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (dao.deleteUsuario(id)){
+
+                            Toast.makeText(PerfilUsuario.this, "Se eliminó Correctamente", Toast.LENGTH_SHORT).show();
+                            Intent a = new Intent(PerfilUsuario.this, login.class);
+                            startActivity(a);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(PerfilUsuario.this, "ERROR: No fue posible eliminar su cuenta", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                b.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                b.show();
                 break;
             case R.id.btnSalirPerf:
                 Intent c=new Intent(PerfilUsuario.this, login.class);
--- app/src/main/java/com/example/entregadecomida/actualizarUsuario.java
+++ app/src/main/java/com/example/entregadecomida/actualizarUsuario.java
@@ -2,13 +2,87 @@ package com.example.entregadecomida;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class actualizarUsuario extends AppCompatActivity {
+public class actualizarUsuario extends AppCompatActivity implements View.OnClickListener {
+
+    EditText actNombres, actApellidos, actUsuario, actPassword;
+    Button btnActualizar, btnCancelar;
+    int id = 0;
+    Usuario u;
+    daoUsuario dao;
+    Intent x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actualizar_usuario);
+
+        actNombres = (EditText) findViewById(R.id.txtNombreReg);
+        actApellidos = (EditText) findViewById(R.id.txtApellidoReg);
+        actUsuario = (EditText) findViewById(R.id.txtUsuarioReg);
+        actPassword = (EditText) findViewById(R.id.txtContraseñaReg);
+        btnActualizar = (Button) findViewById(R.id.btnEditActualizar);
+        btnCancelar = (Button) findViewById(R.id.btnCancelarReg);
+        btnActualizar.setOnClickListener(this);
+        btnCancelar.setOnClickListener(this);
+
+        Bundle b = getIntent().getExtras();
+        id = b.getInt("id");
+        dao = new daoUsuario(this);
+        u = dao.getUsuariobyID(id);
+        actUsuario.setText(u.getUsuario());
+        actPassword.setText(u.getPassword());
+        actNombres.setText(u.getNombres());
+        actApellidos.setText(u.getApellidos());
+
+
     }
+
+    public void onClick(View view){
+
+        switch (view.getId()){
+
+            case R.id.btnEditActualizar:
+                u.setUsuario(actUsuario.getText().toString());
+                u.setPassword(actPassword.getText().toString());
+                u.setNombres(actNombres.getText().toString());
+                u.setApellidos(actApellidos.getText().toString());
+
+                if(!u.isNull()){
+
+                    Toast.makeText(this, "ERROR: Campos Vacios", Toast.LENGTH_SHORT).show();
+
+                }
+                else if (dao.updateUsuario(u)){
+
+                    Toast.makeText(this, "Actualización Exitosa!", Toast.LENGTH_SHORT).show();
+                    Intent x = new Intent(actualizarUsuario.this, PerfilUsuario.class);
+                    x.putExtra("id", u.getId());
+                    startActivity(x);
+                    finish();
+
+                }
+                else {
+
+                    Toast.makeText(this, "Error al Actualizar", Toast.LENGTH_SHORT).show();
+                    
+                }
+                break;
+
+            case R.id.btnCancelarReg:
+                Intent z = new Intent(actualizarUsuario.this, PerfilUsuario.class);
+                z.putExtra("id", u.getId());
+                startActivity(z);
+                finish();
+                break;
+        }
+
+    }
+
 }
